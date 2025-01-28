78edff6840489eef106c91ec44d69d8e588c23ec
karan11062004 <karan.arora.11.a@gmail.com>
2025-01-28 20:15:41 +0530

    save changes

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("DJANGO_SECURITY_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost','127.0.0.1:8000','*']
 STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
 
 
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
